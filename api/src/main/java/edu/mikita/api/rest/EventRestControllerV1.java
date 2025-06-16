de60a18fb3046db7a219e715a66c9eb2971b8b63
slipk <slipknoshka777@gmail.com>
2025-06-16 23:27:09 +0200

    add UI module and implement full page caching strategy

--- api/src/main/java/edu/mikita/api/rest/EventRestControllerV1.java
+++ api/src/main/java/edu/mikita/api/rest/EventRestControllerV1.java
@@ -5,6 +5,8 @@ import edu.mikita.api.service.EventService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
@@ -30,5 +32,10 @@ public class EventRestControllerV1 {
     @DeleteMapping("/{id}")
     public void delete(@PathVariable String id) {
         eventService.delete(id);
+    }
+
+    @GetMapping
+    public List<EventDto> getAll() {
+        return eventService.getAll();
     }
 }
